57e702a4b9bfd68e7478d498a3ab30ce255998f7
Steven Bagshaw <steve@shoreinformatics.com>
2019-12-21 13:18:50 +1100

    Multi-line cells and rewrite range renderer

--- src/app/services/grid.service.ts
+++ src/app/services/grid.service.ts
@@ -16,22 +16,34 @@ export class GridService {
     };
   }
 
-  getZeroFixedLineCellRenderer(rangeEnd: number, isIncludeValueText?: boolean) {
+  getRangedLineCellRenderer(rangeStart: number, rangeEnd: number, startProperty: string, endProperty: string, isIncludeValueText?: boolean, nullReplacement?: string) {
     return params => {
+      const d = params.data;
+      const isFakeHeader = d.isFakeHeader;
+      const dStart = +d[startProperty];
 
       const eDiv = document.createElement('div');
       eDiv.className = 'centredCell fullHeightCell';
 
-      if (params.data.isFakeHeader) {
+      // if (!dStart) {
+      //   if (nullReplacement && !params.data.isSubCategoryRow) { eDiv.innerHTML = nullReplacement; }
+      //   return eDiv;
+      // }
+
+      if (isFakeHeader) {
         eDiv.innerHTML = '<div class="squeeze">' +
-          `<div class="segmentLabel floatDown">${ rangeEnd / 2 }</div><div class="segmentLabel floatUp float-left">0</div><div class="segmentLabel float-right floatUp">${ rangeEnd }</div>` +
+          `<div class="segmentLabel floatDown">${ Math.round((rangeEnd - rangeStart) / 2) + rangeStart }</div><div class="segmentLabel floatUp float-left">${ rangeStart }</div><div class="segmentLabel float-right floatUp">${ rangeEnd }</div>` +
         '</div>';
       } else {
-        const dEnd = +params.value;
-        const percentPerIncrement = 100 / rangeEnd;
-        const width = dEnd * percentPerIncrement;
-        const suffix = rangeEnd === 100 ? '%' : '';
-        const valueText = isIncludeValueText && !params.data.isFakeHeader ? `<div>${ dEnd }${ suffix }</div>` : '';
+        const dEnd = +d[endProperty];
+        const percentPerIncrement = 100 / (rangeEnd - rangeStart);
+        const marginLeft = +(dStart - rangeStart) * percentPerIncrement;
+        const width = (dEnd - dStart) * percentPerIncrement;
+        let valueText = '';
+
+        if (isIncludeValueText) {
+          valueText = !dStart && nullReplacement ? `<div>${ nullReplacement }</div>` : `<div>${ dStart } - ${ dEnd }</div>`;
+        }
 
         const markerLines =
         '<div class="row squeeze">' +
@@ -40,7 +52,7 @@ export class GridService {
         '</div>';
 
         const line = params.data.isSubCategoryRow ? '' :
-          `<div style="width: ${ width }%; border-bottom: 3px solid goldenrod; margin:-22px 0 0 1px;"></div>${ valueText }`;
+          `<div style="margin-left: 1px;"><div style="margin-left: ${ marginLeft }%; margin-top: -22px; width: ${ width }%; border-bottom: 4px solid goldenrod;"></div></div>${ valueText }`;
 
         eDiv.innerHTML = `${ markerLines }${ line }`;
       }
@@ -49,43 +61,39 @@ export class GridService {
     };
   }
 
-  getRangedLineCellRenderer(rangeStart: number, rangeEnd: number, startProperty: string, endProperty: string, isIncludeValueText?: boolean, nullReplacement?: string) {
-    return params => {
-      const d = params.data;
-      const isFakeHeader = d.isFakeHeader;
+  // getZeroFixedLineCellRenderer(rangeEnd: number, isIncludeValueText?: boolean) {
+  //   return params => {
 
-      const dStart = +d[startProperty];
-      const dEnd = +d[endProperty];
+  //     const eDiv = document.createElement('div');
+  //     eDiv.className = 'centredCell fullHeightCell';
 
-      const eDiv = document.createElement('div');
-      eDiv.className = 'centredCell';
+  //     if (params.data.isFakeHeader) {
+  //       eDiv.innerHTML = '<div class="squeeze">' +
+  //         `<div class="segmentLabel floatDown">${ rangeEnd / 2 }</div><div class="segmentLabel floatUp float-left">0</div><div class="segmentLabel float-right floatUp">${ rangeEnd }</div>` +
+  //       '</div>';
+  //     } else {
+  //       const dEnd = +params.value;
+  //       const percentPerIncrement = 100 / rangeEnd;
+  //       const width = dEnd * percentPerIncrement;
+  //       const suffix = rangeEnd === 100 ? '%' : '';
+  //       const valueText = isIncludeValueText && !params.data.isFakeHeader ? `<div>${ dEnd }${ suffix }</div>` : '';
 
-      if (!dStart) {
-        if (nullReplacement && !params.data.isSubCategoryRow) { eDiv.innerHTML = nullReplacement; }
-        return eDiv;
-      }
+  //       const markerLines =
+  //       '<div class="row squeeze">' +
+  //        '<span class="col-3 qtr">&nbsp;</span><span class="col-3 qtr">&nbsp;</span>' +
+  //        '<span class="col-3 qtr">&nbsp;</span><span class="col-3 qtr4">&nbsp;</span>' +
+  //       '</div>';
 
-      const percentPerIncrement = 100 / (rangeEnd - rangeStart);
-      const marginLeft = (dStart - rangeStart) * percentPerIncrement;
-      const width = (dEnd - dStart) * percentPerIncrement;
-      let valueText = '';
+  //       const line = params.data.isSubCategoryRow ? '' :
+  //         `<div style="width: ${ width }%; border-bottom: 3px solid goldenrod; margin:-22px 0 0 1px;"></div>${ valueText }`;
 
-      if (isFakeHeader) {
-        valueText = `<div><div class="float-left">${ d[startProperty] }</div><div class="float-right">${ d[endProperty] }</div></div>`;
-      } else if (isIncludeValueText) {
-        valueText = `<div>${ d[startProperty] } - ${ d[endProperty] }</div>`;
-      }
+  //       eDiv.innerHTML = `${ markerLines }${ line }`;
+  //     }
 
-      // console.log('pppi percentPerIncrement', percentPerIncrement)
-      // console.log('pppi marginLeft', marginLeft, isFakeHeader, d[startProperty], dStart, rangeStart)
-      // console.log('pppi width', width)
-      // console.log('pppi xx d', dStart, dEnd)
-      // console.log('pppi xx range', rangeStart, rangeEnd)
+  //     return eDiv;
+  //   };
+  // }
 
-      eDiv.innerHTML = `<div style="margin-left: ${ marginLeft }%; width: ${ width }%; border-bottom: 3px solid darkgreen;"></div>${ valueText }`;
-      return eDiv;
-    };
-  }
 
   // fancier boolean cell renderer (green tick, red cross)
   getBooleanCellRenderer() {
@@ -124,6 +132,7 @@ export class GridService {
 
       if (params.data.isFakeHeader) { classes = 'fakeHeader '; }
       if (params.colDef.isDataCentred) { classes += 'centredCell '; }
+      if (params.colDef.isDataMultiline) { classes += 'multilineCell '; }
       if (params.colDef.isSubHeadingInColumn && !params.data.isFakeHeader && typeof params.value !== 'number') { classes += 'subHeadingInRow '; }
 
       return classes;
